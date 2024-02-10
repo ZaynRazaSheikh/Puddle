7e9ed5fd6e9181f778098aed8668ebd3ddf03294
ZaynRazaSheikh <z.sheikh125@gmail.com>
2024-02-10 05:53:44 +0500

    added related items fiels

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -5,7 +5,11 @@ from .models import Item
 # Create your views here.
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(Category=item.Category, is_sold=False).exclude(pk=pk)[:3]
+
+
 
     return render(request, 'item\detail.html', {
-        'item': item
+        'item': item,
+        'related_items': related_items
     })
